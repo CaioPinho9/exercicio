b07b7d5d55f15c77bfcb30c828a0c6b5423bfde9
CaioPinho9 <69582667+CaioPinho9@users.noreply.github.com>
2022-03-03 18:25:16 -0300

    Hello World

--- exercicio/src/Exercise.java
+++ exercicio/src/Exercise.java
@@ -1,6 +1,20 @@
 public class Exercise {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+
+        String nome = "Caio Broering Pinho";
+        long matricula = 802191841;
+
+        System.out.println("-------------\n" +
+                            "Hello World\n"  +
+                            "-------------\n"+
+                            "Nome: " + nome + "\n " +
+                            "Número de Matrícula: " + matricula + "\n" +
+                            "Telefone: 984348103" + "\n" +
+                            "UNISUL\n" +
+                            "Universidade do Sul de Santa Catarina\n" +
+                            "Aluno: " + nome + "\n " +
+                            "Curso: Sistemas de informação\n" +
+                            "Matrícula: " + matricula);
     }
 }
